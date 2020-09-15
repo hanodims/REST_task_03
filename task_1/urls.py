ba74dd2c2a0da054a72542d9050c750e3bfd496a
hanodims <hend--mohammed@outlook.com>
2020-09-15 05:04:57 +0300

    rename url = login

--- task_1/urls.py
+++ task_1/urls.py
@@ -10,7 +10,7 @@ from rest_framework_simplejwt.views import (
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('login/', TokenObtainPairView.as_view(), name='login'),
     path('api/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
     path('flights/', views.FlightsList.as_view(), name="flights-list"),
